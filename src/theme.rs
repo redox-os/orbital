342e21999a371fd480d428ddeabbaea6e80700b3
Jeremy Soller <jackpot51@gmail.com>
2017-02-20 22:27:28 -0700

    Use arc theme

--- src/theme.rs
+++ src/theme.rs
@@ -1,7 +1,7 @@
 use orbclient::Color;
 
 pub const BACKGROUND_COLOR: Color = Color::rgb(75, 163, 253);
-pub const BAR_COLOR: Color = Color::rgb(40, 45, 57);
-pub const BAR_HIGHLIGHT_COLOR: Color = Color::rgb(80, 86, 102);
-pub const TEXT_COLOR: Color = Color::rgb(204, 210, 224);
+pub const BAR_COLOR: Color = Color::rgb(47, 52, 63);
+pub const BAR_HIGHLIGHT_COLOR: Color = Color::rgb(0x44, 0x4a, 0x58);
+pub const TEXT_COLOR: Color = Color::rgb(0xD3, 0xDA, 0xE3);
 pub const TEXT_HIGHLIGHT_COLOR: Color = Color::rgb(235, 241, 255);
