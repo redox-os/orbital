545542a5c7166b29df44311a1096b17e731684bd
Wildan M <willnode@wellosoft.net>
2026-07-17 23:21:21 +0700

    Send ResizeEvent at window init for redraw request

--- src/scheme.rs
+++ src/scheme.rs
@@ -389,6 +389,9 @@ impl OrbitalScheme {
         Self::update_window(&mut self.compositor, window, |_compositor, window| {
             let w = w.unwrap_or(window.width());
             let h = h.unwrap_or(window.height());
+            if w == window.width() && h == window.height() {
+                return;
+            }
             window.set_size(w, h);
         });
 
@@ -1797,6 +1800,9 @@ impl OrbitalScheme {
                 }
                 .to_event(),
             );
+        } else {
+            // needed by winit to send the first redraw event
+            window.event(ResizeEvent { height, width }.to_event());
         }
 
         self.windows.insert(id, window);
